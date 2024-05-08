Fixed masking

r_facelib/utils/face_restoration_helper.py
@@ -191,7 +191,7 @@ class FaceRestoreHelper(object):
             self.all_landmarks_5.append(landmark)
             self.det_faces.append(bbox[0:5])
             
-        logger.info(f"Faces detected = {len(self.det_faces)}")
+        logger.status(f"Faces detected = {len(self.det_faces)}")
 
         if len(self.det_faces) == 0:
             return 0
@@ -300,6 +300,11 @@ class FaceRestoreHelper(object):
                 input_img = self.pad_input_imgs[idx]
             else:
                 input_img = self.input_img
+
+            # With this code, we extract the cropped face at native resolution, and then downscale it to 128x128.
+            # After downscaling, we upscale w/ bicubic to self.face_size. This approach avoids upscaling the original
+            # inswapper_128 face twice, and results in significantly better clarity especially with higher face
+            # resolutions!
             cropped_face = cv2.warpAffine(
                 input_img, affine_matrix / scale_factor, [int(i/scale_factor) for i in self.face_size],
                 borderMode=border_mode, borderValue=(135, 133, 132), flags=cv2.INTER_NEAREST)  # gray
@@ -309,7 +314,7 @@ class FaceRestoreHelper(object):
             save_image(cropped_face, "downscaled-cropped")
             cropped_face = cv2.resize(cropped_face, self.face_size, interpolation=cv2.INTER_CUBIC)
             self.cropped_faces.append(cropped_face)
-            save_image(cropped_face, "cropped")
+            save_image(cropped_face, "fullsize-cropped")
 
             # save the cropped face
             if save_cropped_path is not None:
@@ -421,7 +426,6 @@ class FaceRestoreHelper(object):
                                    int(2 * self.upscale_factor * 10)), np.uint8))
             pasted_face = inv_mask_erosion[:, :, None] * inv_restored
             total_face_area = np.sum(inv_mask_erosion)  # // 3
-            logger.status(f"total face area = {total_face_area}")
             # add border
             if draw_box:
                 h, w = face_size
@@ -456,9 +460,12 @@ class FaceRestoreHelper(object):
                 MASK_COLORMAP = [0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 255, 0, 0, 0]
                 for idx, color in enumerate(MASK_COLORMAP):
                     parse_mask[out == idx] = color
-                #  blur the mask
-                parse_mask = cv2.dilate(parse_mask, np.ones((w_edge, w_edge), np.uint8))
-                parse_mask = cv2.GaussianBlur(parse_mask, (101, 101), 11)
+                # blur the mask
+                # Since Reactor Masking Helper is a thing, we'd rather be more liberal with the masking area to maintain
+                # clarity on face edges
+                parse_mask = cv2.dilate(parse_mask, np.ones((12, 12), np.uint8))
+                parse_mask = cv2.GaussianBlur(parse_mask, (11, 11), 11)
+
                 # remove the black borders
                 thres = 10
                 parse_mask[:thres, :] = 0
@@ -479,7 +486,6 @@ class FaceRestoreHelper(object):
                 upsample_img = inv_soft_mask * pasted_face + (1 - inv_soft_mask) * upsample_img[:, :, 0:3]
                 upsample_img = np.concatenate((upsample_img, alpha), axis=2)
             else:
-                logger.status(f"no alpha")
                 upsample_img = inv_soft_mask * pasted_face + (1 - inv_soft_mask) * upsample_img
                 save_image(inv_soft_mask * pasted_face, "pasted face")
 

scripts/reactor_swapper.py
@@ -196,7 +196,7 @@ def in_swap(img, bgr_fake, M, aimg):
     save_image(bgr_fake, "swapped")
 
     scale_factor = np.sqrt(sum([np.square(M[0][0]), np.square(M[1][0])]))
-    logger.status(f"M scale = {scale_factor}")
+    logger.info(f"M scale = {scale_factor}")
 
     target_img = img
     fake_diff = bgr_fake.astype(np.float32) - aimg.astype(np.float32)
@@ -207,6 +207,8 @@ def in_swap(img, bgr_fake, M, aimg):
     fake_diff[:, -2:] = 0
     IM = cv2.invertAffineTransform(M)
     img_white = np.full((aimg.shape[0], aimg.shape[1]), 255, dtype=np.float32)
+
+    # We use NEAREST interpolation here to allow for lossless downsampling in the face restoration step
     bgr_fake = cv2.warpAffine(bgr_fake, IM, (target_img.shape[1], target_img.shape[0]), borderValue=0.0,
                               flags=cv2.INTER_NEAREST)
     img_white = cv2.warpAffine(img_white, IM, (target_img.shape[1], target_img.shape[0]), borderValue=0.0,
@@ -376,7 +378,6 @@ def swap_face(
                         target_face, wrong_gender = get_face_single(target_img, target_faces, face_index=face_num, gender_target=gender_target, order=faces_order[0])
                         if target_face is not None and wrong_gender == 0:
                             logger.status(f"Swapping...")
-                            logger.status(f"source face = {source_face.shape}")
                             aimg, _ = face_align.norm_crop2(result, target_face.kps, 128)
                             bgr_fake, m = face_swapper.get(result, target_face, source_face, paste_back=False)
                             result, M_scale = in_swap(result, bgr_fake, m, aimg)